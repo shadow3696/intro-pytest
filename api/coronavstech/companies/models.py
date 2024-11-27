9b984b6ec77a8fbffbd73469d279e059965ffd0d
shadow3696 <alialemi.droid21@gmail.com>
2024-11-27 15:11:46 +0330

    format with black

--- api/coronavstech/companies/models.py
+++ api/coronavstech/companies/models.py
@@ -10,7 +10,9 @@ class Company(models.Model):
         HIRING = "Hiring"
 
     name = models.CharField(max_length=30, unique=True)
-    status = models.CharField(max_length=30, choices=CompanyStatus.choices, default=CompanyStatus.HIRING)
+    status = models.CharField(
+        max_length=30, choices=CompanyStatus.choices, default=CompanyStatus.HIRING
+    )
     last_updated = models.DateTimeField(default=now, editable=True)
     application_link = URLField(blank=True)
     notes = models.CharField(max_length=100, blank=True)
