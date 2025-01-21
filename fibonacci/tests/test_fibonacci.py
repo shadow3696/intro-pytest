1e122bb73760bdf6899a6ce6367a74273a19251c
shadow3696 <alialemi.droid21@gmail.com>
2025-01-21 10:59:34 +0330

    add dynamic.py and make better test

--- fibonacci/tests/test_fibonacci.py
+++ fibonacci/tests/test_fibonacci.py
@@ -2,24 +2,25 @@ from typing import Callable
 import pytest
 
 from fibonacci.cached import fibonacci_cached, fibonacci_lru_cached
+from fibonacci.dynamic import fibonacci_dynamic, fibonacci_dynamic_v2
 from fibonacci.naive import fibonacci_naive
 from fixtures import time_tracker
 
-@pytest.mark.parametrize(
-    "n,excepted",
-    [
-        (0, 0),
-        (1, 1),
-        (2, 1),
-        (20, 6765),
-    ],
-)
-def test_naive(n: int, excepted: int) -> None:
-    res = fibonacci_naive(n=n)
-    assert res == excepted
+# @pytest.mark.parametrize(
+#     "n,excepted",
+#     [
+#         (0, 0),
+#         (1, 1),
+#         (2, 1),
+#         (20, 6765),
+#     ],
+# )
+# def test_naive(n: int, excepted: int) -> None:
+#     res = fibonacci_naive(n=n)
+#     assert res == excepted
 
 
-@pytest.mark.parametrize("fib_func", [fibonacci_naive, fibonacci_cached, fibonacci_lru_cached])
+@pytest.mark.parametrize("fib_func", [fibonacci_naive, fibonacci_cached, fibonacci_lru_cached, fibonacci_dynamic, fibonacci_dynamic_v2])
 @pytest.mark.parametrize(
     "n,excepted",
     [
